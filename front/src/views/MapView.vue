64f9d7bfe96607559b156e202bedbcd382a53c18
Jeremy SIMEON <jeremy.simeon@external.engie.com>
2025-04-29 10:41:16 +0200

    Add typography + informations

--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-col gap-4">
     <AppCard>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt iusto nam, quo ratione ab reiciendis quam atque pariatur laborum nesciunt deserunt facere! Est unde voluptate magni rerum aliquid ratione!
+      <div class="prose max-w-none">
+        <p>The website should provide an endpoint that returns a geojson file with building consumptions calculated.</p>
+        <p>The front will display the colors of the buildings according to the consumption.</p>
+      </div>
     </AppCard>
     <div id="map" class="w-full" style="height: 500px" />
   </div>
@@ -14,7 +17,7 @@ import AppCard from '@/components/AppCard.vue';
 
 onMounted(() => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGs2MDcxIiwiYSI6ImNtOXd0YTFzZDB1d2QyanIwNXU3dnByZGgifQ.hpO_NTVNfIzC6lVIaSNaTg';
-  // Put mapbox code here !
   
-})
+  // Put mapbox code here !
+});
 </script>
