295727eb4d933257f472606d93df357cc163d2f4
FT6532 <FT6532@ENGIE.COM>
2025-04-29 17:30:20 +0200

    add front

--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -18,6 +18,34 @@ import AppCard from '@/components/AppCard.vue';
 onMounted(() => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGs2MDcxIiwiYSI6ImNtOXd0YTFzZDB1d2QyanIwNXU3dnByZGgifQ.hpO_NTVNfIzC6lVIaSNaTg';
   
-  // Put mapbox code here !
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [2.35, 48.85],
+    zoom: 9,
+  });
+
+  map.on('load', () => {
+        map.addSource('buildings', {
+            type: 'geojson',
+            // Use a URL for the value for the `data` property.
+            data: 'https://localhost:7118/api/buildings.geojson'
+});
+
+        map.addLayer({
+            'id': 'buildings-layer',
+            'type': 'fill',
+            'source': 'buildings',
+            'paint': {
+                'fill-color':
+                  ['interpolate',
+                    ['linear'],
+                    ['get', 'consumption'],
+                    0, 'red',
+                    6_000_000, 'green'
+                  ],
+            },
+        });
+      });
 });
 </script>
