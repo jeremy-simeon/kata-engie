0faf15dd40e9a0dad7cc986d6ef398a4644b3b57
Jeremy SIMEON <jeremy.simeon@external.engie.com>
2025-04-29 10:43:01 +0200

    Format front code

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col gap-4">
     <AppCard>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt iusto nam, quo ratione ab reiciendis quam atque pariatur laborum nesciunt deserunt facere! Est unde voluptate magni rerum aliquid ratione!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt iusto nam, quo ratione
+      ab reiciendis quam atque pariatur laborum nesciunt deserunt facere! Est unde voluptate magni
+      rerum aliquid ratione!
     </AppCard>
     <div class="grid grid-cols-2 gap-4">
       <AppCard>
--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col gap-4">
     <AppCard>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt iusto nam, quo ratione ab reiciendis quam atque pariatur laborum nesciunt deserunt facere! Est unde voluptate magni rerum aliquid ratione!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt iusto nam, quo ratione
+      ab reiciendis quam atque pariatur laborum nesciunt deserunt facere! Est unde voluptate magni
+      rerum aliquid ratione!
     </AppCard>
     <div id="map" class="w-full" style="height: 500px" />
   </div>
@@ -14,7 +16,7 @@ import AppCard from '@/components/AppCard.vue';
 
 onMounted(() => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGs2MDcxIiwiYSI6ImNtOXd0YTFzZDB1d2QyanIwNXU3dnByZGgifQ.hpO_NTVNfIzC6lVIaSNaTg';
-  
+
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
@@ -23,26 +25,27 @@ onMounted(() => {
   });
 
   map.on('load', () => {
-        map.addSource('buildings', {
-            type: 'geojson',
-            // Use a URL for the value for the `data` property.
-            data: 'https://localhost:7118/api/buildings.geojson'
-});
+    map.addSource('buildings', {
+      type: 'geojson',
+      data: 'https://localhost:7118/api/buildings.geojson',
+    });
 
-        map.addLayer({
-            'id': 'buildings-layer',
-            'type': 'fill',
-            'source': 'buildings',
-            'paint': {
-                'fill-color':
-                  ['interpolate',
-                    ['linear'],
-                    ['get', 'consumption'],
-                    0, 'red',
-                    6_000_000, 'green'
-                  ],
-            },
-        });
-      });
-})
+    map.addLayer({
+      id: 'buildings-layer',
+      type: 'fill',
+      source: 'buildings',
+      paint: {
+        'fill-color': [
+          'interpolate',
+          ['linear'],
+          ['get', 'consumption'],
+          0,
+          'red',
+          6_000_000,
+          'green',
+        ],
+      },
+    });
+  });
+});
 </script>
